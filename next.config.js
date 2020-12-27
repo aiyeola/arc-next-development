acbb464df204bd831007e80016dbba9e5cdba96c
Victor Aiyeola <aiyeolavictor@gmail.com>
2020-12-27 10:29:47 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      /* config for next-optimized-images */
-    },
-  ],
+module.exports = {
+  target: 'serverless',
+  hoc: () =>
+    withPlugins([
+      [
+        optimizedImages,
+        {
+          /* config for next-optimized-images */
+        },
+      ],
 
-  // your other plugins here
-]);
-// target: "serverless",
+      // your other plugins here
+    ]),
+};
